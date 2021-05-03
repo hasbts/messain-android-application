31104e358807ba0b0e36ee8b3d4cb2f147838909
KENZA AMANI <kenza.amani@etud.iga.ac.ma>
2021-05-03 15:41:12 +0000

    delete a  comment

--- app/src/main/java/com/example/messageapp/RegisterActivity.java
+++ app/src/main/java/com/example/messageapp/RegisterActivity.java
@@ -26,7 +26,6 @@ public class RegisterActivity extends AppCompatActivity {
     TextView mLoginBtn;
     FirebaseAuth fAuth;
     ProgressBar progressBar;
-    //comment
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
